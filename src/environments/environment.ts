5ec5b7b3547f4464215517f9fba0bc454a22c710
Mark Maxwell <ilosethegame116@gmail.com>
2018-07-01 15:46:04 -0400

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDz1zw2a3Lpd8MbDwk3guvNJAlQEadLkwA",
+    authDomain: "clientpanelprod-b381d.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-b381d.firebaseio.com",
+    projectId: "clientpanelprod-b381d",
+    storageBucket: "clientpanelprod-b381d.appspot.com",
+    messagingSenderId: "1082202616198"
+  }
 };
