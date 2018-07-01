78f50801bed729c1ecf91c6e0909d97cef0ae864
Mark Maxwell <ilosethegame116@gmail.com>
2018-07-01 16:33:37 -0400

    folder structure change

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDz1zw2a3Lpd8MbDwk3guvNJAlQEadLkwA",
+    authDomain: "clientpanelprod-b381d.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-b381d.firebaseio.com",
+    projectId: "clientpanelprod-b381d",
+    storageBucket: "clientpanelprod-b381d.appspot.com",
+    messagingSenderId: "1082202616198"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDz1zw2a3Lpd8MbDwk3guvNJAlQEadLkwA",
+    authDomain: "clientpanelprod-b381d.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-b381d.firebaseio.com",
+    projectId: "clientpanelprod-b381d",
+    storageBucket: "clientpanelprod-b381d.appspot.com",
+    messagingSenderId: "1082202616198"
+  }
+};
